4b12c2c1419336d81bf4cb7d1a7987b6b3787ddc
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-01-23 18:48:03 +0530

    rebuilding site Thu 23 Jan 2020 06:48:03 PM IST

--- deploy.sh
+++ deploy.sh
@@ -23,5 +23,21 @@ git push origin master
 
 yes | cp -rf ./public/* ../animesh-chouhan.github.io
 
+# Update main dir.
+cd ../animesh-chouhan.github.io
+
+# Add changes to git.
+git add .
+
 # Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+printf "\033[0;32mDone\033[0m\n"
 
